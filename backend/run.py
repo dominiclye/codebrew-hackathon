f9eae32c0686859f597e7db2ce7689faa6359291
dominiclye <dominiclye91@gmail.com>
2024-04-04 19:16:00 +1100

    rough authentication system

--- backend/run.py
+++ backend/run.py
@@ -2,3 +2,16 @@
 # Copyright 2024 Dominic Lye, Justin Pirie, Riley Plumridge
 # Execution point of the flask application, initializes all of the files.
 
+from flask import Flask, request
+from api.auth.routes import auth_routes
+from config import db, app
+from flask_login import LoginManager
+
+
+auth_routes(app, db)
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
