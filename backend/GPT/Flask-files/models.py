f50201649405a1322f1a535f4ac54f48760725b3
rqlq <rplum24@icloud.co>
2024-04-04 17:34:28 +1100

    updated models and routes for user info

--- backend/GPT/Flask-files/models.py
+++ backend/GPT/Flask-files/models.py
@@ -1,16 +1,16 @@
 from config import db
 
 
-class Contact(db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(80), unique=False, nullable=False)
-    last_name = db.Column(db.String(80), unique=False, nullable=False)
+    username = db.Column(db.String(80), unique=False, nullable=False)
+    password = db.Column(db.String(80), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
 
     def to_json(self):
         return {
             "id": self.id,
-            "firstName": self.first_name,
-            "lastName": self.last_name,
+            "username": self.username,
+            "password": self.password,
             "email": self.email,
         }
